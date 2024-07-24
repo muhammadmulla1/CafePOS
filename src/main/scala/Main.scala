0537967187553931915232fb06fe6a0a67d63cb3
Muhammad Mulla <muhammad.mulla@mercator.group>
2024-07-24 11:21:55 +0100

    adding to list

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,18 +2,14 @@ object Main extends App {
 
   case class CafeMenu(name: String, price: Double, isHot: Boolean, isFood: Boolean, premium: Boolean)
 
-
-
   val list: List[CafeMenu] = List(
-  CafeMenu("cake", 1.50, isHot = true, isFood = true, premium = true)
+    CafeMenu("cake", 1.50, isHot = true, isFood = true, premium = true)
   )
 
-
-  def addPremiumSpecial(specialItem: String, menu: List[CafeMenu]): List = {
-
-    menu:+ specialItem
-
+  def addPremiumSpecial(specialItem: CafeMenu, menu: List[CafeMenu]): List[CafeMenu] = {
+    menu :+ specialItem
   }
 
-
+  val newItem = CafeMenu("premium coffee", 3.00, isHot = true, isFood = false, premium = true)
+  val updatedMenu = addPremiumSpecial(newItem, list)
 }
